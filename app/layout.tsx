6fc2daea4d40ffc5332da3ade1615d72324c0095
Sushanth Reddy <suri868877@gmail.com>
2026-04-17 18:18:12 +0530

    favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,10 +36,14 @@ export const metadata: Metadata = {
   publisher: "Shivamani Dental",
 
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
-  },
+  icon: [
+    { url: "/favicon.ico" },
+    { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    { url: "/favicon.png", sizes: "512x512", type: "image/png" },
+  ],
+  apple: "/apple-touch-icon.png",
+},
 
   robots: {
     index: true,
